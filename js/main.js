fc4c56eff98d1ec38f76b93218aba8515a044b8d
Beyond DAO <14137098+RetroSoftLabs@users.noreply.github.com>
2024-11-30 19:18:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2780,8 +2780,9 @@
                     this.foreground.children.sort((e, t) => (e = e.gameData).size === (t = t.gameData).size ? e.id - t.id : e.size - t.size)
                 }
                 addCell(e) {
-                    this.foreground.addChild(e);
-                    console.log(e);
+                    //this.foreground.addChild(e);
+                    //console.log(e);
+                    return
                 }
                 addFood(e) {
                     this.food.addChild(e)
