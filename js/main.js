76a2bf531464d0c3a2fa9be19406f836b01f7e35
RetroSoftLabs <RetroSoftLabs@users.noreply.github.com>
2024-12-03 02:40:08 +0300

    fix log

--- js/main.js
+++ js/main.js
@@ -3774,9 +3774,9 @@
                             t.writeUInt8(21), t.writeUInt8(0), n.connection.send(t, e)
                             console.log("alive");
                         }
-                    }, 120)), e ? (n.isAlive(!1) || o.autoRespawning || n.actions.join(), n.activePid = n.playerId, this.focused = !1) : (n.isAlive(!0) || this.autoRespawning || this.spawn(), n.activePid = this.pid, this.focused = !0), this.updateOutlines()
+                    }, 120)), e ? (n.isAlive(!1) || o.autoRespawning || n.actions.join(), n.activePid = n.playerId, this.focused = !1) : (n.isAlive(!0) || this.autoRespawning || this.spawn(), n.activePid = this.pid, this.focused = !0), this.updateOutlines();
                 console.log(`e ? (n.isAlive(!1), o.autoRespawning) : (n.isAlive(!0) , this.autoRespawning , "this.spawn()")`);
-                e ? console.log(n.isAlive(!1), o.autoRespawning) : console.log(n.isAlive(!0) , this.autoRespawning , "this.spawn()"));
+                e ? console.log(n.isAlive(!1), o.autoRespawning) : console.log(n.isAlive(!0) , this.autoRespawning , "this.spawn()");
                 }
             }
         }, function(e, t, s) {}, function(e) {
